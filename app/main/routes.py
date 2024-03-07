78571a12db74abf756576376491133057db9e593
cesarcalafrioli <cesarmoes@gmail.com>
2024-03-07 00:22:58 -0300

    Teste de troca do frictionless pelo requests

--- app/main/routes.py
+++ app/main/routes.py
@@ -21,14 +21,16 @@ class Portal:
 
         for dataset in range(len(response_package["result"])):
             self.datasets.append(response_package["result"][dataset])
-            self.datasets2.append(Dataset(url, response_package["result"][dataset]))
+            self.datasets2.append(Dataset(self.url, response_package["result"][dataset]))
 
-        print("Lista dos conjuntos de dados aos quais pertencem a este portal: "+str(self.datasets))
-        print("Informação do primeiro conjunto de dados instanciado")
-        print("----------------------------------------------------")
-        print("Título: "+self.datasets2[0].titulo)
-        print("Quantidade de resources: "+str(self.datasets2[0].qtd_resource))
-        #print("Id das resources:"+str(self.datasets2.))
+        
+        # TESTE - DATASETS
+        #print("Lista dos conjuntos de dados aos quais pertencem a este portal: "+str(self.datasets))
+        #print("Informação do primeiro conjunto de dados instanciado")
+        #print("----------------------------------------------------")
+        #print("Título: "+self.datasets2[0].titulo)
+        #print("Quantidade de resources: "+str(self.datasets2[0].qtd_resource))
+        
 
 # Extrai as informações do Conjunto de dados
 class Dataset:
@@ -37,16 +39,27 @@ class Dataset:
         self.dataset_id = dataset_id
         self.dataset_resources = []
         
-        full_url = 'https://{}/dataset/'.format(url)
+        #full_url = 'https://{}/dataset/'.format(self.url)
 
-        ckan_control = CkanControl()
-        package = Package(full_url+dataset_id, control=ckan_control)
+        #ckan_control = CkanControl()
+        #package = Package(full_url+self.dataset_id, control=ckan_control)
 
-        self.titulo = package.title
-        self.qtd_resource = len(package.resources)
-                
-        # Lista todas as resources contidas no dataset
+        #self.titulo = package.title
+        #self.qtd_resource = len(package.resources)
 
+        # Listando todos os resources
+        #for resource in range(self.qtd_resource):
+            #self.dataset_resources.append(Resource(self.url, ))
+            #pass
+
+        self.full_url = requests.get('https://{}/api/3/action/package_show?id={}'.format(self.url, self.dataset_id))
+        self.response = self.full_url.json()
+
+        self.qtd_resources = len(self.response["result"]["resources"])
+        print("Resources do conjunto de dados {}: ".format(self.response["result"]["title"]))
+        for resource in range(self.qtd_resources):
+            self.dataset_resources.append(Resource(self.url, self.dataset_id))
+            
 
 # Extrai as informações do resource do conjunto de dados
 class Resource:
@@ -54,26 +67,21 @@ class Resource:
         self.url = url
         self.resource_id = resource_id
 
-        full_url = 'https://{}/api/3/action/datastore_search?resource_id={}'.format(url,resource_id)
+        resource_url = requests.get('https://{}/api/3/action/datastore_search?resource_id={}'.format(self.url, self.resource_id))
+        response = resource_url.json()
 
-
-
-    
+        
 @bp.route('/', methods=['GET'])
 def index():
     header = "Frictionless - Demo"
-
-    #dataset1 = Dataset('boletim-oficial')
-    #dataset2 = Dataset('cursos-de-graduacao')
-
-    #ds_list = [dataset1, dataset2]
-    #print("Info das datasets")
-    #print(ds_list[0].titulo)
 
     portal = Portal('dados.ufpe.br')
 
     ds_list = portal.datasets2
 
+    # TESTE - ANALISANDO UM RESOURCE EM PARTICULAR ###
+
+    """
     url = requests.get('https://dados.ufpe.br/api/3/action/datastore_search?resource_id=cd9e4b1b-b1bc-47a5-9055-7ccdf31a072a')
     response = url.json()
     print(response["help"])
@@ -85,6 +93,14 @@ def index():
         print("Campos desta resource:"+str(response["result"]["fields"][field]["id"]))
         print("Campos desta resource:"+str(response["result"]["fields"][field]))
         print("Descrição:"+str(response["result"]["fields"][field]["info"]['notes']))
-
+    ###################################################
+        """
 
     return render_template('index.html', titulo='Frictionless demo', datasets=ds_list, header=header, portal=portal)
+
+# Exibindo informações de um dataset
+@bp.route('/<string:dataset_id>/', methods=('GET','POST'))
+def dataset(dataset_id):
+    dataset = Dataset('dados.ufpe.br', dataset_id)
+
+    return render_template('dataset.html', dataset=dataset)    
